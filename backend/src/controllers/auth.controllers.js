6dbff429de1d8afc37c7922c9517e19724893e8c
anishraj836 <anish9771928408@outlook.com>
2025-06-24 05:10:53 +0530

    Fix upsertUserStream

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -121,7 +121,7 @@ export async function onboard(req,res) {
             isOnboarded: true,
         },{new: true});
         if(!updatedUser) return res.status(404).json({message: "User not found"});
-        try {await upsertStreamUser({
+        try {await createStreamUser({
             id: updatedUser._id.toString(),
             name: updatedUser.fullName,
             image: updatedUser.profilePic||"",
